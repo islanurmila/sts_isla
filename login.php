3764a66b489b81e877a2544eb6da27d6042fd864
islanurmila <isla.nurmila@gmail.com>
2023-09-30 11:47:38 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,17 @@
+<h1>Isla Nurmila RPL 3</h1>
+<?php
+$username = 12345;
+$password = 12345;
+
+//If bercabang
+If($username == 12345 && $password == 12345){
+   echo "anda berhasil login";
+}else if ($username == 22222 && $password = 22222){
+   echo "user name salah";
+}else if ($username == 22222 && $password == 22222){
+    echo "mohon maaf username dan password salah";
+}else{
+    echo "password salah";
+}
+
+?>
